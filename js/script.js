630a37c80eb108e20b6ebd042f8cdc2b28f01429
Muhammad Umer Khan <146569788+umerkhan8028@users.noreply.github.com>
2024-11-10 15:08:58 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal( '.home-content p, .about-content' ,{ origin: 'right' });
 /*==================== typed js ====================*/
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Frontend Developer', 'Digital Marketing', 'Backend Developer'],
+    strings: ['Frontend Developer', 'SEO Expert', 'Backend Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
